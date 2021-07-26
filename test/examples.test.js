b5dcd3f49321c77e0fc72e923610513708f74715
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-26 21:17:38 +0200

    Update tests
    also closes #2

--- test/examples.test.js
+++ test/examples.test.js
@@ -1,71 +1,71 @@
-const MappedReplacer = require("../index");
-const assert = require("chai").assert;
+const MappedReplacer = require('../mapped-replacer')
+const assert = require('chai').assert
 
-describe("examples", () => {
-	describe("addRule()", () => {
-		it('should return "Hello world 😀"', () => {
-			const mapper = new MappedReplacer();
+describe('examples', () => {
+  describe('addRule()', () => {
+    it('should return "Hello world 😀"', () => {
+      const mapper = new MappedReplacer()
 
-			mapper.addRule(":smile:", "😀");
-			assert.equal(mapper.replace("Hello world :smile:"), "Hello world 😀");
-		});
-	});
+      mapper.addRule(':smile:', '😀')
+      assert.equal(mapper.replace('Hello world :smile:'), 'Hello world 😀')
+    })
+  })
 
-	describe("addRules()", () => {
-		it('should return "&#120139; &#8776; &#120113;"', () => {
-			const mapper = new MappedReplacer();
+  describe('addRules()', () => {
+    it('should return "&#120139; &#8776; &#120113;"', () => {
+      const mapper = new MappedReplacer()
 
-			mapper.addRules({
-				"𝕋": "&#120139;",
-				"≈": "&#8776;",
-				"𝔱": "&#120113;",
-			});
+      mapper.addRules({
+        '𝕋': '&#120139;',
+        '≈': '&#8776;',
+        '𝔱': '&#120113;',
+      })
 
-			assert.equal(mapper.replace("𝕋 ≈ 𝔱"), "&#120139; &#8776; &#120113;");
-		});
-	});
+      assert.equal(mapper.replace('𝕋 ≈ 𝔱'), '&#120139; &#8776; &#120113;')
+    })
+  })
 
-	describe("removeRule()", () => {
-		it('should "𝕋 &#8776; 𝔱"', () => {
-			const mapper = new MappedReplacer();
+  describe('removeRule()', () => {
+    it('should "𝕋 &#8776; 𝔱"', () => {
+      const mapper = new MappedReplacer()
 
-			mapper.addRule("𝕋", "&#120139;");
-			mapper.addRule("≈", "&#8776;");
+      mapper.addRule('𝕋', '&#120139;')
+      mapper.addRule('≈', '&#8776;')
 
-			mapper.removeRule("𝕋");
+      mapper.removeRule('𝕋')
 
-			assert.equal(mapper.replace("𝕋 ≈ 𝔱"), "𝕋 &#8776; 𝔱");
-		});
-	});
+      assert.equal(mapper.replace('𝕋 ≈ 𝔱'), '𝕋 &#8776; 𝔱')
+    })
+  })
 
-	describe("rulesCount()", () => {
-		it("should return 1", () => {
-			const mapper = new MappedReplacer();
+  describe('rulesCount()', () => {
+    it('should return 1', () => {
+      const mapper = new MappedReplacer()
 
-			mapper.addRule("𝕋", "&#120139;");
+      mapper.addRule('𝕋', '&#120139;')
 
-			assert.equal(mapper.rulesCount(), 1);
-		});
-	});
+      assert.equal(mapper.rulesCount(), 1)
+    })
+  })
 
-	describe("clearRules()", () => {
-		it("shouldn return 0", () => {
-			const mapper = new MappedReplacer();
+  describe('clearRules()', () => {
+    it('shouldn return 0', () => {
+      const mapper = new MappedReplacer()
 
-			mapper.addRule("𝕋", "&#120139;");
-			mapper.clearRules();
+      mapper.addRule('𝕋', '&#120139;')
+      mapper.clearRules()
 
-			assert.equal(mapper.rulesCount(), 0);
-		});
-	});
+      assert.equal(mapper.rulesCount(), 0)
+    })
+  })
 
-	describe("replace()", () => {
-		it('should return "a &#8594; b"', () => {
-			const mapper = new MappedReplacer();
+  describe('replace()', () => {
+    it('should return "a &#8594; b"', () => {
+      const mapper = new MappedReplacer()
 
-			mapper.addRule("→", "&#8594;");
+      mapper.addRule('→', '&#8594;')
 
-			assert.equal(mapper.replace("a → b"), "a &#8594; b");
-		});
-	});
-});
+      assert.equal(mapper.replace('a → b'), 'a &#8594; b')
+    })
+  })
+})
