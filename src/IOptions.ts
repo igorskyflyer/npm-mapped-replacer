38d0c705a79073d0f75f907c0817a08747347c41
igorskyflyer <igor.dvlpr@gmail.com>
2024-07-28 03:19:43 +0200

    Add strict mode and JSDocs

--- src/IOptions.ts
+++ src/IOptions.ts
@@ -1,5 +1,18 @@
 // Author: Igor Dimitrijević (@igorskyflyer)
 
 export interface IOptions {
+  /**
+   * A Boolean that indicates whether replacing should be case-sensitive or not.
+   *
+   * Default is `true`.
+   */
   caseSensitive?: boolean
+  /**
+   * A Boolean that indicates whether strict mode is enabled.
+   *
+   * In strict mode, only whole matches are replaced.
+   *
+   * Default is `false`.
+   */
+  strict?: boolean
 }
